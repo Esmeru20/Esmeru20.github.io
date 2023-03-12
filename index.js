3643fba032e049054821c5c8b519c30cea7afcfa
Esmeralda Seoane <esmeralda19seoane97@gmail.com>
2023-03-12 03:53:41 -0300

    repositorio_v2_proyectofinal_cv

--- index.js
+++ index.js
@@ -8,12 +8,42 @@ const setTheme = (theme) => {
     localStorage.setItem('theme', theme);
 }
 
-slider.addEventListener('click', ()  => {
+slider.addEventListener('click', () => {
     let switchToTheme = localStorage.getItem('theme') === 'dark' ? 'light' : 'dark';
     setTheme(switchToTheme);
 });
 
 setTheme(localStorage.getItem('theme') || preferedColorScheme);
 
-// Article //
+// Article - 01 Niñera //
 
+const element1 = document.getElement1ById("myBtn");
+element.addEventListener("click", myFunction);
+
+function myFunction() {
+    document.getElement1ById("demo").innerHTML = "Entretenimiento. Cuidado personal y psicologico.";
+}
+
+/** Article - 02 Productora Gráfica 
+
+const element2 = document.getElement2ById("mybebe");
+element.addEventListener("click", myFunction);
+
+function myFunction() {
+    document.getElement2ById("vemo").innerHTML = "Ilustración vectorial, diseño. Atención al público y con proveedores (presencial y remota).";
+}
+
+**/
+
+ooooo
+
+/** Article - 03 Asistente juridico 
+
+const element3 = document.getElement3ById("myBtn");
+element.addEventListener("click", myFunction);
+
+function myFunction() {
+    document.getElement3ById("nemo").innerHTML = "Confección de documentación. Entrega de legajos y gestión de tramites. Digitalización y mantenimiento de documentos digitales.";
+}
+
+**/
